7b0c8e9642670780e1cbd6216474ec3de171f07f
mani <22hr1a3262@mtieat.org>
2024-12-09 22:28:29 +0530

    operators.py

--- operators.py
+++ operators.py
@@ -11,7 +11,8 @@ class Main{
         }
 }
 """
-
+unary
+"""
 class Main{
     public static void main(String[] args){
         int x=10;
@@ -21,3 +22,63 @@ class Main{
         System.out.println(x--);
         }
 }
+"""
+shift
+"""
+class Main{
+public static void main(String[] args){
+ System.out.println(10<<2);
+ System.out.println(20<<2);
+ System.out.println(20<<3);
+}
+}
+"""
+logical AND and bitwise &
+"""
+class Main{
+public static void main(String[] args){
+int a=10;
+int b=5;
+int c=20;
+System.out.println(a<b&&a<c);
+System.out.println(a<b&a<c);
+}
+}
+"""
+logical OR and bitwise OR
+"""
+class Main{
+public static void main(String[] args){
+int a=10;
+int b=5;
+int c=20;
+System.out.println(a>b||a>c);
+System.out.println(a>b|a>c);
+}
+}
+"""
+ternary
+"""
+class Main{
+public static void main(String[] args){
+int a=2;
+int b=5;
+int min=(a<b)?a:b;
+System.out.println(min);/System.out.println(max);
+}
+}
+"""
+assignment
+"""
+class Main{
+public static void main(String[] args){
+int a=10;
+int b=20;
+a+=b;
+System.out.println(a);
+a-=b;
+System.out.println(a);
+}
+}
+"""
+
