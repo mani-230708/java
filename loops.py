178e0a25820b2c23eae733f6993fbaa41dc2b926
mani <22hr1a3262@mtieat.org>
2024-12-09 21:33:23 +0530

    loops.py

---
+++ loops.py
@@ -0,0 +1,49 @@
+for
+"""
+class main{
+public static void main(string[] args){
+for(i=0;i<5;i++)
+{
+System.out.println("MK");
+}
+}
+}
+"""
+while
+"""
+class main{
+public static void main(string[] args){
+int i=1;
+while(i<=5)
+{
+System.out.println(i);
+}
+}
+}
+"""
+do while
+"""
+class main{
+public static void main(string[] args){
+int i=5;
+do
+{
+System.out.println(i);
+i--;
+}
+while(i>=1)
+}
+}
+"""
+for each loop
+"""
+class main{
+public static void main(string[] args){
+int arr[]={1,2,3,4,5};
+for(int values:arr)
+{
+System.out.println(values);
+}
+}
+}
+
