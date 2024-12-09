964a4f3dff599127b4725d4fb32c5b511c60e37d
mani <22hr1a3262@mtieat.org>
2024-12-09 20:17:14 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,23 @@
+arithmetic operators
+"""
+class Main{
+    public static void main(String[] args){
+        int x=10,y=4;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        System.out.println(x/y);
+        }
+}
+"""
+
+class Main{
+    public static void main(String[] args){
+        int x=10;
+        System.out.println(x++);
+        System.out.println(--x);
+        System.out.println(++x);
+        System.out.println(x--);
+        }
+}
