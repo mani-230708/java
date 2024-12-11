948a288b8792b84816a7906b7305a461fe0938b1
mani <22hr1a3262@mtieat.org>
2024-12-11 11:50:29 +0530

    arrange.py

---
+++ arrange.py
@@ -0,0 +1,13 @@
+"""
+import java.util.Arrays;
+public class g {
+
+	public static void main(String[] args) {
+		int arr[]= {1,4,2,8,6};
+		System.out.println(Arrays.toString(arr));
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
+"""
