b9ebc47f975e5e41670bcecc54a9b7bbd0f9ee3d
mani <22hr1a3262@mtieat.org>
2024-12-11 14:29:14 +0530

    sring.py

---
+++ sring.py
@@ -0,0 +1,52 @@
+space count
+"""
+public class i {
+
+	public static void main(String[] args) {
+		int count=0;
+		String words="Hello good morning";
+		for(int i=0;i<words.length();i++)
+		{
+			if(words.charAt(i)==' ')
+			{
+				count++;
+			}
+		}
+		System.out.println(count+1);
+	}
+
+}
+"""
+vowal count
+"""
+public class j {
+
+	public static void main(String[] args) {
+		int vowelcount=0;
+		String words="Hello good morning";
+		for(int i=0;i<words.length();i++)
+		{
+			char ch=words.charAt(i);
+
+			if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') 
+			{
+				vowelcount++;
+			}
+		}
+		System.out.println(vowelcount);
+	}
+
+}
+"""
+words count
+"""
+public class j {
+   public static void main(String[] args) {
+   String words="Hello good morning";
+   System.out.println(words.length());
+   }
+}
+"""
+
+   
+
