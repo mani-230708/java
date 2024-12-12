6461813a4cc026e4af16ce0dd537964037e15b44
mani <22hr1a3262@mtieat.org>
2024-12-12 13:57:31 +0530

    mor.py

---
+++ mor.py
@@ -0,0 +1,32 @@
+"""
+class bank{
+	int getRateofIterest(){
+		return 0;
+		}
+}
+class SBI extends bank{
+	int getRateofIterest(){
+		return 9;
+		}
+}class AXIS extends bank{
+	int getRateofIterest(){
+		return 8;
+		}
+}
+class HDFC extends bank{
+	int getRateofIterest(){
+		return 7;
+		}
+}
+public class mor {
+
+	public static void main(String[] args) {
+		SBI b1=new SBI();
+		AXIS b2=new AXIS(); 
+		HDFC b3=new HDFC();
+		System.out.println("the rate of interest of SBI:"+b1.getRateofIterest());
+		System.out.println("the rate of interest of AXIS:"+b2.getRateofIterest());
+		System.out.println("the rate of interest of HDFC:"+b3.getRateofIterest());
+	}
+}
+"""
