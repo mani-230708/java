900eebe4b3d39f13b40832563c684182db8cb246
mani <22hr1a3262@mtieat.org>
2024-12-13 14:47:29 +0530

    chaining const.py

--- chaining const.py
+++ chaining const.py
@@ -1,3 +1,4 @@
+constructor chaining
 """
 public class Parent {
 	Parent(){
@@ -14,6 +15,67 @@ public class SampleCode {
 
 	public static void main(String[] args) {
 		Child c11=new Child();
+	}
+
+}
+"""
+local chaining
+"""
+public class Human {
+	private String Name;
+	private String Location;
+	private int Age;
+	private double Height;
+	private double Weight;
+	Human(String a,String b,int c,double d,double e){
+		this( "ramu","banglore",21,176.9);
+		Name=a;
+		Location=b;
+		Age=c;
+		Height=d;
+		Weight=e;
+	}
+	Human(String a,String b,int c,double d){
+		this( "shamu","hyderabad",20);
+		Name=a;
+		Location=b;
+		Age=c;
+		Height=d;
+	}
+	Human(String a,String b,int c){
+		Name=a;
+		Location=b;
+		Age=c;
+	}
+	
+	public String getName() {
+		return Name;
+	}
+	public String getLocation() {
+		return Location;
+	}
+	public int getAge() {
+		return Age;
+		
+	}
+	public double getHeight() {
+		return Height;
+	}
+	public double getWeight() {
+		return Weight;
+	}
+	
+}
+public class LaunchAgain {
+
+	public static void main(String[] args) {
+		Human h=new Human("sunny","chennai",19,156.7,65);
+		System.out.println(h.getName());
+		System.out.println(h.getLocation());
+		System.out.println(h.getAge());
+		System.out.println(h.getHeight());
+		System.out.println(h.getWeight());
+
 	}
 
 }
