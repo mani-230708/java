eb3d5dd3005e5215ee12235dbaf0c917b9ff2f02
mani <22hr1a3262@mtieat.org>
2024-12-18 22:12:19 +0530

    arraylist.py

---
+++ arraylist.py
@@ -0,0 +1,38 @@
+"""
+import java.util.ArrayList;
+
+public class sample {
+	public static void main(String[] args) {
+		ArrayList a1=new ArrayList();
+		a1.add(30);
+		a1.add(20);
+		a1.add("mk"+","+"km");
+		a1.add(true);
+		a1.add(1.43);
+		System.out.println(a1);
+		ArrayList a2=new ArrayList();
+		a2.add(50);
+		a2.add("mlk");
+		a2.add(false);
+		a2.add(14.32);
+		a2.add("start");
+		System.out.println(a2);
+		ArrayList a3=new ArrayList();
+		a3.add(62);
+		a3.add("MLK");
+		a3.add(true);
+		a3.add(14.320);
+		a3.add(1);
+		a3.add("continue");
+		System.out.println(a3);
+		a1.remove(4);
+		a2.remove(3);
+		a3.remove(4);
+		System.out.println(a1);
+		System.out.println(a2); 
+		System.out.println(a3);
+	}
+	
+
+}
+"""
